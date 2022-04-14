e45673efad1bb91902ffd2672a83a23112ac8445
Cristina Pelepenko <pelepenko.cristina@gmail.com>
2022-04-13 21:03:45 -0300

    correct merge conflict

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//COMPONENTES
+import Home from "./components/home";
+import ComponentA from "./components/component-a";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+      //Este código abaixo serve para remover o header que está aparecendo acima de tudo,
+      //deixei comentado para vocês verem, assim que verificarem o redirecionamento, podemos remover.
+      // screenOptions={{
+      //   headerShown: false,
+      // }}
+      >
+        {/* Stack.Screen serve para criar os componentes para serem acessados
+        posteriomente */}
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ComponentA" component={ComponentA} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-Rent-a-lab
+# Rent-a-lab
+
+## Instalando Ambiente
+
+Para rodar o projeto é necessário atualizar o expo para podermos utilizar o expo install. Para isso, é necessário executar os seguintes comandos:
+
+```
+yarn add expo-cli@5.3.1
+```
+
+```
+expo update
+```
+
+Após isto podemos instalar o pacote ReactNavigation utilizando o seguinte comando:
+
+```
+yarn add @react-navigation/native @react-navigation/native-stack
+```
+
+```
+expo install react-native-screens react-native-safe-area-context
+```
