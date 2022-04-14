4eb67e09810c3cc5b83bdf9b3cbe620213527e26
Cristina Pelepenko <pelepenko.cristina@gmail.com>
2022-04-13 22:14:02 -0300

    feat: Criar fluxo de telas

---
+++ components/reserve-solicitation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import Styles from "../styles/main-style";
+
+const SolicitacaoReserva = ({ navigation }) => {
+  return (
+    <View style={Styles.container}>
+      <Text>Listar aqui todas solicitações de reservas</Text>
+    </View>
+  );
+};
+
+export default SolicitacaoReserva;
