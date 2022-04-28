777c6cd0b014fb2f51c47b3a48153cd666b222a6
Cristina Pelepenko <pelepenko.cristina@gmail.com>
2022-04-27 21:06:34 -0300

    colocar espaço entre botões

--- components/start-teacher.jsx
+++ components/start-teacher.jsx
@@ -9,17 +9,25 @@ const InicioProfessor = ({ navigation }) => {
     <View style={Styles.container}>
       <Text>Listar aqui todas as reservas</Text>
 
+      <View style={{ flex: 0.1 }} />
+
       <Button
         title="Ver Reservas"
         onPress={() => navigation.navigate("VerReserva")}
       />
 
+      <View style={{ flex: 0.1 }} />
+
       <Text>Listar aqui todas as minhas reservas</Text>
+
+      <View style={{ flex: 0.1 }} />
 
       <Button
         title="Cadastrar Reserva"
         onPress={() => navigation.navigate("CriarReserva")}
       />
+
+      <View style={{ flex: 0.1 }} />
 
       <Button title="Sair" onPress={() => navigation.navigate("Login")} />
     </View>
