a208790041c46fea56ee30d75803ff3039595236
Cristina Pelepenko <pelepenko.cristina@gmail.com>
2022-06-08 22:28:28 -0300

    tratar cadastro de lab gravando em banco

--- source/labs.js
+++ source/labs.js
@@ -1,12 +1,12 @@
 import firebase from "./firebase-config";
-import { getDatabase, ref, get } from "firebase/database";
+import { getDatabase, ref, get, push } from "firebase/database";
 
 const db = getDatabase();
+const labsRef = ref(db, "labs");
 
 async function getLabs() {
   const labs = new Array();
 
-  const labsRef = ref(db, "labs");
   await get(labsRef).then((snapshot) => {
     snapshot.forEach((childSnapshot) => {
       labs.push(childSnapshot.val());
@@ -16,4 +16,12 @@ async function getLabs() {
   return labs;
 }
 
-export { getLabs };
+async function registerLaboratory(lab) {
+  await push(labsRef, {
+    name: lab.name,
+    id: lab.id,
+    description: lab.description,
+  });
+}
+
+export { getLabs, registerLaboratory };
